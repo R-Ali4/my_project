f2a5de10c10ac607a083ae103dfcc7f6103dfcd1
Raman Ali <raman@MacBook-Air-van-Raman.local>
2024-10-27 14:01:40 +0100

    Add space

--- python_1.py
+++ python_1.py
@@ -5,11 +5,12 @@
 # integers
 print(2)
 print("type of 2 is", type(2))
+
 # floats
 print(2.5)
 print("type of 2.5 is" , type(2.5))
 
-#complex
+# complex
 #complex numbers are written with a "j" as the imaginary part
 print(2+3j)
 print("type of (2+3j) is",type(2+3j))
