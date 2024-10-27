d6ffdf8987c4f071dd578739813bf7ce9e307eff
Raman Ali <raman@MacBook-Air-van-Raman.local>
2024-10-27 11:48:09 +0100

    Initial commit

---
+++ python_1.py
@@ -0,0 +1,15 @@
+# data types in python3
+
+#1. numbers
+
+# integers
+print(2)
+print("type of 2 is", type(2))
+# floats
+print(2.5)
+print("type of 2.5 is" , type(2.5))
+
+#complex
+#complex numbers are written with a "j" as the imaginary part
+print(2+3j)
+print("type of (2+3j) is",type(2+3j))
