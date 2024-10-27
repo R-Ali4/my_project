bf5f7fa40eadc7b9dd338396e583b93bff869430
Raman Ali <raman@MacBook-Air-van-Raman.local>
2024-10-27 13:00:21 +0100

    Add string

--- python_1.py
+++ python_1.py
@@ -13,3 +13,6 @@ print("type of 2.5 is" , type(2.5))
 #complex numbers are written with a "j" as the imaginary part
 print(2+3j)
 print("type of (2+3j) is",type(2+3j))
+
+#2. strings
+print("Hello, world")
